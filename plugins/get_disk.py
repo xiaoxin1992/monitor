7c24bfad185cca104cf5f516570ee27dbd6ceda4
root <root@test1.localdomain>
2015-04-11 22:40:54 +0800

    get_disk.py update

--- plugins/get_disk.py
+++ plugins/get_disk.py
@@ -23,10 +23,12 @@ def GETDISK_SIZE(device_path=""):
     """
     if not device_path:
         return 'error'
-    #SIZE = {}
     SIZE_CACHE=1024*1024
     DISK_INFO = ""
-    DISK_INFO = os.statvfs(device_path)
+    try:
+        DISK_INFO = os.statvfs(device_path)
+    except OSError,e:
+        return "Error:%s" % e
     DISK_Free = (DISK_INFO.f_bavail * DISK_INFO.f_frsize)
     DISK_Total = (DISK_INFO.f_blocks * DISK_INFO.f_frsize)
     DISK_Use = (DISK_INFO.f_blocks - DISK_INFO.f_bfree) * DISK_INFO.f_frsize
@@ -34,13 +36,22 @@ def GETDISK_SIZE(device_path=""):
     DISK_Total = MB_TO_GB(float(DISK_Total)/SIZE_CACHE)
     DISK_Use = MB_TO_GB(float(DISK_Use)/SIZE_CACHE)
     DISK_Free = MB_TO_GB(float(DISK_Free)/SIZE_CACHE)
-    return {
-        'Total':DISK_Total,
-        'Use':DISK_Use,
-        'Free':DISK_Free,
-        'Usage':[DISK_Usage,'%']
-    }
-    #return SIZE
+    DISK_IO_Total = DISK_INFO.f_files
+    DISK_IO_Use =  DISK_INFO.f_files - DISK_INFO.f_ffree 
+    DISK_IO_Free = DISK_INFO.f_ffree
+    DISK_IO_Usage = round(float(DISK_IO_Use) / float(DISK_IO_Total) * 100,1)
+    TMP_DISK = ({
+        'DISK_Total':DISK_Total,
+        'DISK_Use':DISK_Use,
+        'DISK_Free':DISK_Free,
+        'DISK_Usage':[DISK_Usage,'%']}
+    ,{
+        'DISK_IO_Total':DISK_IO_Total,
+        'DISK_IO_Use':DISK_IO_Use,
+        'DISK_IO_Free':DISK_IO_Free,
+        'DISK_IO_Usage':DISK_IO_Usage
+    })
+    return TMP_DISK
 def GETDISK():
     """
         获取磁盘名称，磁盘类型。
@@ -48,16 +59,58 @@ def GETDISK():
         1 文件不存在
         0 文件存在，并获取到了信息
     """
-    DISK_INFO={}
+    DISK_INFO=[]
     if not os.path.exists(DISK_CONF_FILE):
         return 1  
     with open(DISK_CONF_FILE) as f:
         data = f.readlines()
     for x in data:
         data1 = x.split()
-        if data1[2].strip() in DIST_TYPE:
-            #print data1
-            DISK_INFO={}
-            #GETDISK_SIZE(data1[1].strip())#,data1[0],data1[2]
-    return 0
-GETDISK()
+        if  data1[2].strip() in DIST_TYPE:
+            DISK_INFO.append(tuple(data1[0:4]))
+    return  DISK_INFO
+def CHECK_DISK(device="aaa"):
+    """
+    0  正常
+    1  未知
+    2  警告
+    3  危险
+
+    """
+    if not device:
+        return "Error: is Device"
+    partition = GETDISK()
+    #print GETDISK_SIZE(partition[1])
+    DISK_STATUS = {}
+    for x in partition:
+        DISK_Usage1 = GETDISK_SIZE(x[1])[0]
+        DISK_Usage = DISK_Usage1['DISK_Usage']
+        DISK_IO_Usage1 = GETDISK_SIZE(x[1])[1]
+        DISK_IO_Usage = DISK_IO_Usage1['DISK_IO_Usage']
+        if DISK_Usage[0] > 90 and DISK_Usage[0] < 100:  #DISK_IO_Usage > 90 and DISK_IO_Usage < 100:
+            DISK_STATUS[x[0]]={
+                'info':'space use 90%',
+                'status':2
+            }
+        elif DISK_Usage[0] >= 100: #or DISK_IO_Usage >= 100:
+            DISK_STATUS[x[0]]={
+                'info':'space use 100%',
+                'status':3 
+            }
+        else:
+            DISK_STATUS[x[0]]={
+                'info':'space use OK',
+                'status':0
+            }
+    return DISK_STATUS
+            #return 2
+        #elif DISK_Usage[0] >= 100 or DISK_IO_Usage >= 100:
+        #    return 3
+        #else:
+        #    return 0
+    #a = GETDISK_SIZE('/')
+#print 
+print CHECK_DISK()
+#sys.exit(CHECK_STATUS)
+#GETDISK('/dev/sda1','ext4')
+#print GETDISK_SIZE('/')
